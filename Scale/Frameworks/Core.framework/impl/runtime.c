560b96b9cf08a1875fc4e86e025790685911be1f
StonkDragon <theanonym919@gmail.com>
2022-11-20 13:43:44 +0100

    Merge Array, Runtime, and Core Frameworks

--- Scale/Frameworks/Core.framework/impl/runtime.c
+++ Scale/Frameworks/Core.framework/impl/runtime.c
@@ -17,42 +17,65 @@ hash hash1(char* data) {
     return h;
 }
 
-sclDefFunc(Runtime_hasFramework) {
+sclDefFunc(Runtime_hasFramework, scl_int) {
     char* name = ctrl_pop_string();
     for (size_t i = 0; i < scl_internal_frameworks_size; i++) {
         if (strcmp(name, scl_internal_frameworks[i]) == 0) {
-            ctrl_push_long(1);
-            return;
+            return 1;
         }
     }
-    ctrl_push_long(0);
+    return 0;
 }
 
-sclDefFunc(Runtime_listFrameworks) {
+sclDefFunc(Runtime_listFrameworks, void) {
     printf("Frameworks:\n");
     for (size_t i = 0; i < scl_internal_frameworks_size; i++) {
         printf(" %s\n", scl_internal_frameworks[i]);
     }
 }
 
-sclDefFunc(Runtime_getFunctionID) {
+sclDefFunc(Runtime_getFunctionID, scl_int) {
     char* name = ctrl_pop_string();
     hash h = hash1(name);
     
     for (size_t i = 0; i < scl_internal_function_names_size; i++) {
         if (h == scl_internal_function_names[i]) {
-            ctrl_push_long(i);
-            return;
+            return (scl_int) i;
         }
     }
-    ctrl_push_long(-1);
+    return -1;
 }
 
-sclDefFunc(Runtime_callByID) {
+sclDefFunc(Runtime_callByID, void) {
     int id = ctrl_pop_long();
     if (id == -1) {
         fprintf(stderr, "Error: Method not found");
         return;
     }
     scl_internal_function_ptrs[id]();
+}
+
+struct Array {
+    scl_int $__type__;
+    scl_str $__type_name__;
+    scl_value values;
+    scl_value count;
+    scl_value capacity;
+};
+
+extern scl_value alloced_structs[STACK_SIZE];
+extern size_t alloced_structs_count;
+
+sclDefFunc(Runtime_objectsByType, struct Array*) {
+    scl_str type = ctrl_pop_string();
+    struct Array* array = scl_alloc_struct(sizeof(struct Array));
+    array->capacity = (scl_value) alloced_structs_count;
+    array->count = 0;
+    array->values = scl_alloc(alloced_structs_count);
+    for (size_t i = 0; i < alloced_structs_count; i++) {
+        if (strcmp(((struct Array*) alloced_structs[i])->$__type_name__, type) == 0) {
+            ((scl_value*) array->values)[(scl_int) array->count++] = alloced_structs[i];
+        }
+    }
+    return array;
 }
