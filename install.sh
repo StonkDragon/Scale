738ed1cec2520719a3e3843d2432d4e92409e80d
StonkDragon <theanonym919@gmail.com>
2023-03-06 20:05:48 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
-set -xe
-
-curl -o Scale.zip https://github.com/StonkDragon/Scale/archive/refs/tags/v23.0.zip
+wget -o Scale.zip https://github.com/StonkDragon/Scale/archive/refs/tags/v23.0.zip
 unzip Scale.zip
 cd Scale
 sh install_local.sh
