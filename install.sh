75ebe57369ba628e0e19436b4ef7e11e86e71e33
StonkDragon <theanonym919@gmail.com>
2023-03-14 13:39:37 +0100

    Store binary in /opt/Scale and symlink to /usr/local/bin

--- install.sh
+++ install.sh
@@ -1,3 +1,8 @@
+if [[ ":$PATH:" != *":/usr/local/bin:"* ]] && [[ ":$PATH:" != *":/usr/local/bin/:"* ]]; then
+    echo "Your path is missing /usr/local/bin, you might want to add it."
+    exit 1
+fi
+
 if [ `uname` = "Darwin" ]; then
     if ! which gcc >/dev/null; then
         echo "Please install the Xcode command line tools"
@@ -10,20 +15,36 @@ if ! which g++ >/dev/null; then
     exit 1
 fi
 
-if [ ! -e ./Dragon/build/dragon ]; then
+if ! which dragon >/dev/null; then
     echo "---------"
     echo "Downloading dragon..."
     git clone https://github.com/StonkDragon/Dragon
     cd Dragon
+
+    if ! which make >/dev/null; then
+        echo "Please install make"
+        exit 1
+    fi
+
+    echo "Installing dragon. This may require root privileges..."
+
     make compile
     sudo cp build/dragon /usr/local/bin/dragon
     cd ..
+    rm -rf Dragon
 fi
 
 echo "---------"
 echo "Installing..."
 
-./Dragon/build/dragon build -conf install
+dragon build -conf install
+
+for f in /opt/Scale/*; do
+    if [ -e $f/sclc ]; then
+        echo "Linking $f/sclc to sclc-${f:11}"
+        sudo ln -s $f/sclc /usr/local/bin/sclc-${f:11}
+    fi
+done
 
 echo "---------"
 echo "Checking for VSCode installation..."
@@ -34,13 +55,11 @@ if [ -e "$HOME/.vscode/extensions" ]; then
     VSCODE_EXTENSIONS_PATH="$HOME/.vscode/extensions"
 elif [ -e "$HOME/.vscode-insiders/extensions" ]; then
     VSCODE_EXTENSIONS_PATH="$HOME/.vscode-insiders/extensions"
+else
+    echo "No VSCode installation found"
 fi
 
 if [ ! -z "$VSCODE_EXTENSIONS_PATH" ]; then
     echo "Found VSCode extension folder at '$VSCODE_EXTENSIONS_PATH'"
-    echo "Is this correct? [yn]"
-    read answer
-    if [ "$answer" != "${answer#[Yy]}" ]; then
-        cp -r ./scale.vscodeextension "$VSCODE_EXTENSIONS_PATH"
-    fi
+    cp -r ./scale.vscodeextension "$VSCODE_EXTENSIONS_PATH"
 fi
