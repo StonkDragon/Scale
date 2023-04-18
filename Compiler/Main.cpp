2a45b3a0d27fea182ecbf6bd568278a2e5150b1c
StonkDragon <theanonym919@gmail.com>
2023-04-18 20:42:06 +0200

    FUUUU FORGOR TO BUMP VERSION

--- Compiler/Main.cpp
+++ Compiler/Main.cpp
@@ -46,7 +46,7 @@
 #endif
 
 #ifndef FRAMEWORK_VERSION_REQ
-#define FRAMEWORK_VERSION_REQ "23.3"
+#define FRAMEWORK_VERSION_REQ "23.4"
 #endif
 
 #ifndef SCL_ROOT_DIR
@@ -510,7 +510,7 @@ namespace sclc
         DragonConfig::CompoundEntry* framework = new DragonConfig::CompoundEntry();
         framework->setKey("framework");
 
-        framework->addString("version", "23.3");
+        framework->addString("version", "23.4");
         framework->addString("headerDir", "include");
         framework->addString("implDir", "impl");
         framework->addString("implHeaderDir", "impl");
