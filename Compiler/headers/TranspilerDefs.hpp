5283faad199f118f0d6c2fb6ab3ea013929ca2c8
StonkDragon <theanonym919@gmail.com>
2023-04-26 20:39:16 +0200

    remove this old thing here

--- Compiler/headers/TranspilerDefs.hpp
+++ Compiler/headers/TranspilerDefs.hpp
@@ -6,28 +6,4 @@
     err.in = body[(at)].getFile();       \
     err.value = body[(at)].getValue();   \
     err.type = body[(at)].getType();     \
-    std::cout << "TRanspiler error in function " << __func__ << " in line " << __LINE__ << std::endl; \
     err.message = msg
-
-#define resetFile()                                                                          \
-    do                                                                                       \
-    {                                                                                        \
-        if (Main.options.debugBuild)                                                         \
-        {                                                                                    \
-            std::string file = body[i].getFile();                                            \
-            if (strstarts(file, scaleFolder))                                                \
-            {                                                                                \
-                file = file.substr(scaleFolder.size() + std::string("/Frameworks/").size()); \
-            }                                                                                \
-            else                                                                             \
-            {                                                                                \
-                file = std::filesystem::path(file).relative_path();                          \
-            }                                                                                \
-            append("current_file[callstk.ptr - 1] = \"%s\";\n", file.c_str());               \
-            append("current_line[callstk.ptr - 1] = %d;\n", body[i].getLine());              \
-            append("current_col[callstk.ptr - 1] = %d;\n", body[i].getColumn());             \
-            currentFile = file;                                                              \
-            currentLine = body[i].getLine();                                                 \
-            currentCol = body[i].getColumn();                                                \
-        }                                                                                    \
-    } while (0)
