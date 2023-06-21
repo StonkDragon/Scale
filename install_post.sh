c5a7995602001e93779578f2e401f8e431491699
TemperΘΔ <theanonym919@gmail.com>
2023-06-21 20:08:43 +0200

    E

--- install_post.sh
+++ install_post.sh
@@ -8,7 +8,7 @@ ctest
 sudo make install
 
 for f in /opt/Scale/*; do
-    if [ -e $f/sclc ]; then
+    if [ -e $f/sclc ] && [ $f != "/opt/Scale/latest" ]; then
         echo "Linking $f/sclc to sclc-${f:11}"
         sudo rm -rf /usr/local/bin/sclc-${f:11}
         sudo ln -s $f/sclc /usr/local/bin/sclc-${f:11}
