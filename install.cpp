9919125fb0c06df77b56ef9aafedbd8504b6388d
TemperΘΔ <theanonym919@gmail.com>
2023-08-23 14:25:29 +0200

    Update install.cpp

--- install.cpp
+++ install.cpp
@@ -5,6 +5,7 @@ extern "C" {
 
 #include <iostream>
 #include <filesystem>
+#include <cstdlib>
 
 #ifdef _WIN32
 #define DEPEND(_cmd) depend_on_system_command(_cmd " > nul 2>&1")
@@ -12,17 +13,17 @@ extern "C" {
 #define DEPEND(_cmd) depend_on_system_command(_cmd " > /dev/null 2>&1")
 #endif
 void depend_on_system_command(const char* command) {
-    int result = system(command);
+    int result = std::system(command);
     if (result != 0) {
-        fprintf(stderr, "Command '%s' failed with exit code %d\n", command, result);
-        exit(1);
+        std::cerr << "Command '" << command << "' failed with exit code " << result << std::endl;
+        std::exit(1);
     }
 }
 
 #ifdef _WIN32
-#define SOFT_DEPEND(_cmd) system(_cmd " > nul 2>&1")
+#define SOFT_DEPEND(_cmd) std::system(_cmd " > nul 2>&1")
 #else
-#define SOFT_DEPEND(_cmd) system(_cmd " > /dev/null 2>&1")
+#define SOFT_DEPEND(_cmd) std::system(_cmd " > /dev/null 2>&1")
 #endif
 
 int main(int argc, char const *argv[]) {
