624a545bfaa25dc421c1614846cda76deea56ad9
TemperΘΔ <theanonym919@gmail.com>
2023-09-09 09:39:10 +0200

    try fix issue on linux

--- Scale/Internal/scale_runtime.h
+++ Scale/Internal/scale_runtime.h
@@ -380,6 +380,7 @@ typedef struct memory_layout memory_layout_t;
 
 #define				SCL_BACKTRACE(_func_name) \
 						struct _scl_backtrace __scl_backtrace_cur __attribute__((cleanup(_scl_trace_remove))) = { .marker = TRACE_MARKER, .func_name = (_func_name) }; \
+						__asm__ __volatile__("" : : "r" (&__scl_backtrace_cur) : "memory")
 
 void				_scl_trace_remove(const struct _scl_backtrace*);
 
