9867fe28fbcf0fd6998e28b1b1ba8a9648f18c9f
TemperΘΔ <theanonym919@gmail.com>
2024-03-22 15:52:43 +0100

    add pragma! generic

--- Compiler/modules/handlers/Pragma.cpp
+++ Compiler/modules/handlers/Pragma.cpp
@@ -1,3 +1,5 @@
+#include <sstream>
+
 #include "../../headers/Common.hpp"
 #include "../../headers/TranspilerDefs.hpp"
 #include "../../headers/Types.hpp"
@@ -28,6 +30,70 @@ namespace sclc {
                 warns.push_back(err);
             } else {
                 errors.push_back(err);
+            }
+        } else if (body[i].value == "generic") {
+            safeInc();
+            if (!hasVar(body[i].value)) {
+                transpilerError("Unknown variable: '" + body[i].value + "'", i);
+                errors.push_back(err);
+                return;
+            }
+            std::string type = getVar(body[i].value).type;
+            safeInc();
+            if (body[i].type != tok_do) {
+                transpilerError("Expected 'do', but got '" + body[i].value + "'", i);
+                errors.push_back(err);
+                return;
+            }
+            safeInc();
+            bool filledIn = false;
+            while (body[i].type != tok_done) {
+                if (body[i].type == tok_default) {
+                    safeInc();
+                    if (body[i].type != tok_store) {
+                        transpilerError("Expected '=>', but got '" + body[i].value + "'", i);
+                        errors.push_back(err);
+                        return;
+                    }
+                    safeInc();
+                    if (!filledIn) {
+                        handle(Token);
+                        filledIn = true;
+                    } else {
+                        std::ostringstream fp;
+                        handle(Token);
+                        fp.flush();
+                    }
+                } else {
+                    FPResult r = parseType(body, &i);
+                    if (!r.success) {
+                        errors.push_back(r);
+                        return;
+                    }
+                    safeInc();
+                    if (body[i].type != tok_store) {
+                        transpilerError("Expected '=>', but got '" + body[i].value + "'", i);
+                        errors.push_back(err);
+                        return;
+                    }
+                    safeInc();
+                    
+                    if (!filledIn && r.value == type) {
+                        handle(Token);
+                        filledIn = true;
+                    } else {
+                        std::ostringstream fp;
+                        handle(Token);
+                        fp.flush();
+                    }
+                }
+                safeInc();
+            }
+
+            if (body[i].type != tok_done) {
+                transpilerError("Expected 'done', but got '" + body[i].value + "'", i);
+                errors.push_back(err);
+                return;
             }
         } else {
             transpilerError("Unknown pragma '" + body[i].value + "'", i);
